e4e42fbe00486ddae6caf4a1c217d4a6d6e4e7e4
VI Chapmann <vivisoft.hexos.official.mail@gmail.com>
2024-08-04 13:07:55 +0300

    0.1.3-rev65

--- src/main.c
+++ src/main.c
@@ -14,13 +14,13 @@ initrd_t initrd;
 
 void _start()
 {
-    if ((apicID = ACPI_CoreID()) != 0) { while (1); }
+    if (bootboot.bspid != 0) { while (1); }
 
     // for (int i = 0; i < bootboot.fb_width * bootboot.fb_height; i++) (&fb)[i] = 0x1c1c1c;
 
     uart_puts("\n * Octox v" MMP "+rev" MLS " (build " __DATE__ " " __TIME__ ")\n");
     uart_puts(" * ACPI Core ID: 0x");
-    uart_puthex(apicID);
+    uart_puthex(bootboot.bspid);
     uart_puts("\n");
     uart_puts(" * INITRD located at 0x");
     uart_puthex(bootboot.initrd_ptr);

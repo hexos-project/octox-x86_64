d5ed940663d2f8dc9a445c0b6b4a90543dc63492
HexOS Project <vivisoft.hexos.official.mail@gmail.com>
2024-08-04 04:52:37 +0300

    1.0-Alpha0.1.3+mls45_stable

--- src/main.c
+++ src/main.c
@@ -14,41 +14,13 @@ extern unsigned char environment[4096];
 extern u32 fb;
 u64 apicID;
 
-
-void gfx_test() {
-    for (int x = 0; x < bootboot.fb_width; x++)
-        for (int y = 0; y < bootboot.fb_height; y++)
-            gfx_putpixel(x, y, 0x1C1C1C);
-
-    for (int x = 0; x < bootboot.fb_width; x++) {
-        gfx_putpixel(x, 0, 0x00ffffff);
-        gfx_putpixel(x, bootboot.fb_height - 1, 0x00ffffff);
-        gfx_putpixel(x, bootboot.fb_height / 2, 0x00ffffff);
-    }
-
-    for (int y = 0; y < bootboot.fb_height; y++) {
-        gfx_putpixel(0, y, 0x00ffffff);
-        gfx_putpixel(bootboot.fb_width - 1, y, 0x00ffffff);
-        gfx_putpixel(bootboot.fb_width / 2, y, 0x00ffffff);
-    }
-}
-
 void _start()
 {
     if ((apicID = ACPI_CoreID()) != 0) { while (1); }
 
     gfx_init(bootboot, &fb);
 
-    for (int i = 0; i < bootboot.fb_width * bootboot.fb_height; i++)
-        (&fb)[i] = 0x1c1c1c;
-
-    if (GFX_TEST)
-        gfx_test();
-    
-    gfx_blit_chroma(logo_hires_width, logo_hires_height, logo_hires_contents,
-        bootboot.fb_width / 2 - logo_hires_width / 2,
-        bootboot.fb_height / 2 - logo_hires_height / 2,
-        0);
+    // for (int i = 0; i < bootboot.fb_width * bootboot.fb_height; i++) (&fb)[i] = 0x1c1c1c;
 
     uart_puts("\n * HexOS kernel v0.7.1-rev79 (build " __DATE__ " " __TIME__ ")\n");
     uart_puts(" * ACPI Core ID: 0x");

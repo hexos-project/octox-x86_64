3a54596360253b9657bd1d2d4d1e5b3a32507c14
VI Chapmann <vivisoft.hexos.official.mail@gmail.com>
2024-08-07 10:49:14 +0300

    0.1.3+rev106
    some minor changes

--- src/main.c
+++ src/main.c
@@ -12,17 +12,14 @@
 extern BOOTBOOT bootboot;
 extern unsigned char environment[4096];
 extern u32 fb;
-u64 apicID;
 
 void _start()
 {
-    if ((apicID = ACPI_CoreID()) != 0) { while (1); }
-
-    // for (int i = 0; i < bootboot.fb_width * bootboot.fb_height; i++) (&fb)[i] = 0x1c1c1c;
+    if (bootboot.bspid != 0) { while (1); }
 
     uart_puts("\n * Octox v" MMP "+rev" MLS " (build " __DATE__ " " __TIME__ ")\n");
     uart_puts(" * ACPI Core ID: 0x");
-    uart_puthex(apicID);
+    uart_puthex(bootboot.bspid);
     uart_puts("\n");
     uart_puts(" * INITRD located at 0x");
     uart_puthex(bootboot.initrd_ptr);
@@ -35,8 +32,6 @@ void _start()
     uart_puts("\n");
 
     initrd = initrd_init(bootboot);
-
-    uart_puts(" * Starting C++ part\n");
 
     cxx_main();
 

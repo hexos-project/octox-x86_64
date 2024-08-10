9d7732895ee99a9a89cc8b18fe267605cd43f67c
VI Chapmann <vivisoft.hexos.official.mail@gmail.com>
2024-08-10 16:13:50 +0300

    0.1.3+rev265

--- src/sysfn.cc
+++ src/sysfn.cc
@@ -4,18 +4,18 @@
 #include <ihc.h>
 #include <irq.h>
 
-#define COMP_NOP                  0b0000000000000000ULL
-#define COMP_COM_SERIAL_UART_PUTS 0b0001000000000000ULL
-#define COMP_ARCH_IRQ_ON          0b0010000000000000ULL
-#define COMP_ARCH_IRQ_OFF         0b0010000000010000ULL
-#define COMP_ARCH_IHC_SET         0b0010000000010000ULL
-#define COMP_SYSFN_BLOCK          0b0011000000000000ULL
+#define COMP_NOP                  0x0000000000000000ULL
+#define COMP_COM_SERIAL_UART_PUTS 0x0001000000000000ULL
+#define COMP_ARCH_IRQ_ON          0x0002000000000000ULL
+#define COMP_ARCH_IRQ_OFF         0x0002000000010000ULL
+#define COMP_ARCH_IHC_SET         0x0002000100000000ULL
+#define COMP_SYSFN_BLOCK          0x0003000000000000ULL
 
 static bool __justexit = false;
 
 nomangle
 void sysfn_ihc_handler(u64 component, u64 arg1, u64 arg2, u64 arg3, u64 arg4) {
-    uart::cout << " * syscall invoked\n";
+    uart::cout << " * syscall invoked using component " << component << '\n';
     if (__justexit) {
         uart::cout << " * syscall: blocked\n";
         return;

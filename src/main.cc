2e1100a3ac8abe1c11adb98fc80e6ff95dd1a80a
VI Chapmann <vivisoft.hexos.official.mail@gmail.com>
2024-08-10 12:51:30 +0300

    0.1.3+rev255

--- src/main.cc
+++ src/main.cc
@@ -35,7 +35,7 @@ void cxx_main() {
     uart::cout << "Done\n";
 
     uart::cout << " * PIT Initialization, 1 MHz... ";
-    PIT::init(1000000);
+    PIT::init(1000);
     uart::cout << "Done\n";
 
     uart::cout << " * Disabling IRQs... ";
